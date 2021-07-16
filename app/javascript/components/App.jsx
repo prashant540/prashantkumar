80199ec33da907eeb8f1d547b921b7b94aa94ff8
pkumars.540@gmail.com <pkumars.540@gmail.com>
2021-07-16 21:35:15 +0530

    first commit

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Fragment} from 'react';
+import ReactDOM from 'react-dom';
+function App() {
+  return (
+  	<Fragment>
+         <div>
+           <h1> Hello World</h1>
+         </div>
+    </Fragment>
+  );
+}
+
+export default App;
